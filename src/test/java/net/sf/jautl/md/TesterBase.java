0b4ffccb98e4c2a49aff37f75cfc5028230464b1
alexcoppo <alexcoppo@alice.it>
2013-01-15 00:32:28 +0100

    Switch to TestNG

---
+++ src/test/java/net/sf/jautl/md/TesterBase.java
@@ -0,0 +1,49 @@
+/*
+    Copyright (c) 2000-2012 Alessandro Coppo
+    All rights reserved.
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions
+    are met:
+    1. Redistributions of source code must retain the above copyright
+       notice, this list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright
+       notice, this list of conditions and the following disclaimer in the
+       documentation and/or other materials provided with the distribution.
+    3. The name of the author may not be used to endorse or promote products
+       derived from this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+    IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+    OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+    IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+    INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+    NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+    THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+package net.sf.jautl.md;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TesterBase {
+    protected DigestEngine de;
+
+    @Test(dataProvider="test-vectors")
+	public void test(String message, String digest) {
+		de.initiate();
+        
+        if (message != null)
+            de.add(message);
+        else
+            for (int index = 0; index < 1000000; index++)
+                de.add("a");
+                    
+		de.terminate();
+		String result = de.getAsHex().toLowerCase();
+		Assert.assertEquals(result, digest);
+	}
+}
